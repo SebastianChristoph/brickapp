9cf6a637d1f09007dd286e4775bac15b46529b77
SebastianChristoph <sebastianchristoph2@gmail.com>
2025-12-14 22:32:47 +0100

    Styling

--- Data/Services/UserNotificationService.cs
+++ Data/Services/UserNotificationService.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 namespace Data.Services
 {
     public class UserNotificationService
+
+          
     {
         private readonly AppDbContext _db;
         public UserNotificationService(AppDbContext db)
@@ -11,6 +13,29 @@ namespace Data.Services
             _db = db;
         }
 
+
+  public async Task DeleteNotificationAsync(int notificationId)
+            {
+                var notification = await _db.UserNotifications.FindAsync(notificationId);
+                if (notification != null)
+                {
+                    _db.UserNotifications.Remove(notification);
+                    await _db.SaveChangesAsync();
+                }
+            }
+
+        public async Task DeleteAllNotificationsAsync(string userUuid)
+        {
+            var notifications = await _db.UserNotifications
+                .Where(n => n.UserUuid == userUuid)
+                .ToListAsync();
+            if (notifications.Count > 0)
+            {
+                _db.UserNotifications.RemoveRange(notifications);
+                await _db.SaveChangesAsync();
+            }
+        }
+            
         public async Task<List<UserNotification>> GetNotificationsForUserAsync(string userUuid)
         {
             return await _db.UserNotifications
