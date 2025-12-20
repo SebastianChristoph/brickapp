28df90d75af3ca64287841f93a994fe25b578d38
SebastianChristoph <sebastianchristoph2@gmail.com>
2025-12-20 12:25:30 +0100

    add migrations and fix for migrations

---
+++ Migrations/20251220112454_addTestUsers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace brickapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTestUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "Id", "CreatedAt", "IsAdmin", "Name", "Uuid" },
+                values: new object[,]
+                {
+                    { 3, new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), false, "Christian", "333222111" },
+                    { 4, new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), false, "Martin", "444555666" },
+                    { 5, new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), false, "Tim", "555666777" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
