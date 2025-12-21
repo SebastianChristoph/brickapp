38b803d0201eacb556a0a8c9e3d0ace0f16a5a62
SebastianChristoph <sebastianchristoph2@gmail.com>
2025-12-21 20:45:02 +0100

    upload image after

---
+++ Migrations/20251221192712_addNewItemImageReques.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace brickapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewItemImageReques : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "itemImageRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MappedBrickId = table.Column<int>(type: "integer", nullable: false),
+                    RequestedByUserId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    ReasonRejected = table.Column<string>(type: "text", nullable: true),
+                    ApprovedByUserId = table.Column<string>(type: "text", nullable: true),
+                    ApprovedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    TempImagePath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_itemImageRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_itemImageRequests_mappedBricks_MappedBrickId",
+                        column: x => x.MappedBrickId,
+                        principalTable: "mappedBricks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_itemImageRequests_users_ApprovedByUserId",
+                        column: x => x.ApprovedByUserId,
+                        principalTable: "users",
+                        principalColumn: "Uuid",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_itemImageRequests_users_RequestedByUserId",
+                        column: x => x.RequestedByUserId,
+                        principalTable: "users",
+                        principalColumn: "Uuid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_itemImageRequests_ApprovedByUserId",
+                table: "itemImageRequests",
+                column: "ApprovedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_itemImageRequests_MappedBrickId",
+                table: "itemImageRequests",
+                column: "MappedBrickId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_itemImageRequests_RequestedByUserId",
+                table: "itemImageRequests",
+                column: "RequestedByUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "itemImageRequests");
+        }
+    }
+}
