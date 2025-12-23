d0eba9e95e3708f9cbf5f6a5286ea715b2b357ec
SebastianChristoph <sebastianchristoph2@gmail.com>
2025-12-23 21:08:04 +0100

    fix some wanrings

--- Data/Entities/NewSetRequest.cs
+++ Data/Entities/NewSetRequest.cs
@@ -6,16 +6,16 @@ namespace brickapp.Data.Entities
     public class NewSetRequest
     {
         public int Id { get; set; }
-        public string Brand { get; set; }
-        public string SetNo { get; set; }
-        public string SetName { get; set; }
+        public required string Brand { get; set; }
+        public required string SetNo { get; set; }
+        public required string SetName { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public NewSetRequestStatus Status { get; set; } = NewSetRequestStatus.Draft;
         public bool IsDraft => Status == NewSetRequestStatus.Draft;
         public string? ReasonRejected { get; set; }
         public string? PendingReason { get; set; }
-        public string UserId { get; set; }
+        public required string UserId { get; set; }
         public List<NewSetRequestItem> Items { get; set; } = new();
     }
 
@@ -23,9 +23,9 @@ namespace brickapp.Data.Entities
     {
         public int Id { get; set; }
         public int NewSetRequestId { get; set; }
-        public string ItemIdOrName { get; set; }
+        public required string ItemIdOrName { get; set; }
         public int Quantity { get; set; }
-        public string Color { get; set; }
+        public required string Color { get; set; }
     }
 
     public enum NewSetRequestStatus
